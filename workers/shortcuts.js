5e24083ef2f0630f0b3ee0a44a1e47b0752c6436
Filipe Coelho <filipe.coelho@paradox.ai>
2023-09-04 00:05:06 +0300

    Shortcuts injected on multiple cem's pages

--- workers/shortcuts.js
+++ workers/shortcuts.js
@@ -1,65 +1,68 @@
 // Check if the desired element exists
 setTimeout(() => {
-  const targetContainer = document.querySelector(
+  var targetContainer = document.querySelector(
     "header#app-header section.el-container"
   );
-  const integrationCenterComponent = document.querySelector(
+  if (targetContainer == null)
+    targetContainer = document.querySelector(
+      "body.cem-site.desktop.has-toolbar.has-header"
+    );
+
+  let integrationCenterComponent = document.querySelector(
     "div#integration-center-shortcut"
   );
-  const clientSetupIntegrationsComponent = document.querySelector(
+  let clientSetupIntegrationsComponent = document.querySelector(
     "div#client-setup-integrations-shortcut"
   );
-  const candidateInboxComponent = document.querySelector(
+  let candidateInboxComponent = document.querySelector(
     "div#candidate-inbox-shortcut"
   );
 
-  if (targetContainer) {
-    if (integrationCenterComponent == null) {
-      // Fetch the content of the txt file
-      fetch(
-        chrome.runtime.getURL("./components/integration-center-shortcut.html")
-      )
-        .then((response) => response.text())
-        .then((data) => {
-          // Inject the fetched content as the first child of the target container
-          targetContainer.insertAdjacentHTML("afterbegin", data);
-        })
-        .catch((error) => {
-          console.error(
-            "Error fetching the integration-center component file:",
-            error
-          );
-        });
-    }
-    if (clientSetupIntegrationsComponent == null) {
-      // Fetch the content of the txt file
-      fetch(chrome.runtime.getURL("components/client-setup-integrations.html"))
-        .then((response) => response.text())
-        .then((data) => {
-          // Inject the fetched content as the first child of the target container
-          targetContainer.insertAdjacentHTML("afterbegin", data);
-        })
-        .catch((error) => {
-          console.error(
-            "Error fetching the client-setup-integrations component file:",
-            error
-          );
-        });
-    }
-    if (candidateInboxComponent == null) {
-      // Fetch the content of the txt file
-      fetch(chrome.runtime.getURL("components/candidate-inbox-shortcut.html"))
-        .then((response) => response.text())
-        .then((data) => {
-          // Inject the fetched content as the first child of the target container
-          targetContainer.insertAdjacentHTML("afterbegin", data);
-        })
-        .catch((error) => {
-          console.error(
-            "Error fetching the client-setup-integrations component file:",
-            error
-          );
-        });
-    }
+  if (integrationCenterComponent == null) {
+    // Fetch the content of the txt file
+    fetch(
+      chrome.runtime.getURL("./components/integration-center-shortcut.html")
+    )
+      .then((response) => response.text())
+      .then((data) => {
+        // Inject the fetched content as the first child of the target container
+        targetContainer.insertAdjacentHTML("afterbegin", data);
+      })
+      .catch((error) => {
+        console.error(
+          "Error fetching the integration-center component file:",
+          error
+        );
+      });
+  }
+  if (clientSetupIntegrationsComponent == null) {
+    // Fetch the content of the txt file
+    fetch(chrome.runtime.getURL("components/client-setup-integrations.html"))
+      .then((response) => response.text())
+      .then((data) => {
+        // Inject the fetched content as the first child of the target container
+        targetContainer.insertAdjacentHTML("afterbegin", data);
+      })
+      .catch((error) => {
+        console.error(
+          "Error fetching the client-setup-integrations component file:",
+          error
+        );
+      });
+  }
+  if (candidateInboxComponent == null) {
+    // Fetch the content of the txt file
+    fetch(chrome.runtime.getURL("components/candidate-inbox-shortcut.html"))
+      .then((response) => response.text())
+      .then((data) => {
+        // Inject the fetched content as the first child of the target container
+        targetContainer.insertAdjacentHTML("afterbegin", data);
+      })
+      .catch((error) => {
+        console.error(
+          "Error fetching the client-setup-integrations component file:",
+          error
+        );
+      });
   }
 }, 500);
