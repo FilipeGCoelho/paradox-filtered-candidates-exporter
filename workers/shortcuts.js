94dd7009f1955ba889d7eb96a9f4726afffe3ce6
Filipe Coelho <filipe.coelho@paradox.ai>
2023-09-06 11:37:29 +0300

    modified file structure & simplified shortcuts

--- workers/shortcuts.js
+++ workers/shortcuts.js
@@ -1,51 +1,40 @@
+// Array of component filenames
+const componentFiles = [
+  "integration-center-shortcut.html",
+  "client-setup-integrations.html",
+  "candidate-inbox-shortcut.html",
+  "workflows-shortcut.html",
+  "roles-permissions-shortcut.html",
+];
+
 function fetchAndInject(path, target) {
-	fetch(chrome.runtime.getURL(path))
-		.then((response) => response.text())
-		.then((data) => {
-			// Inject the fetched content as the first child of the target container
-			target.insertAdjacentHTML("afterbegin", data);
-		})
-		.catch((error) => {
-			console.error(`Error fetching the ${path} file:`, error);
-		});
+  fetch(chrome.runtime.getURL(path))
+    .then((response) => response.text())
+    .then((data) => {
+      target.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch((error) => {
+      console.error(`Error fetching the ${path} file:`, error);
+    });
 }
 
 // Check if the desired element exists
 setTimeout(() => {
-	var shortcutContainer = document.querySelector("div#shortcut-container");
-	if (shortcutContainer == null) {
-		shortcutContainer = document.createElement("div");
-		shortcutContainer.id = "shortcut-container";
+  let shortcutContainer = document.querySelector("div#shortcut-container");
 
-		var targetContainer = document.querySelector(
-			"header#app-header section.el-container"
-		);
-		if (targetContainer == null) {
-			targetContainer = document.querySelector(
-				"body.cem-site.desktop.has-toolbar.has-header"
-			);
-		}
-		targetContainer = targetContainer.insertAdjacentElement(
-			"afterbegin",
-			shortcutContainer
-		);
+  if (!shortcutContainer) {
+    shortcutContainer = document.createElement("div");
+    shortcutContainer.id = "shortcut-container";
 
-		//integration-center-shortcut
-		fetchAndInject(
-			(path = "./components/integration-center-shortcut.html"),
-			(target = targetContainer)
-		);
-		fetchAndInject(
-			(path = "components/client-setup-integrations.html"),
-			(target = targetContainer)
-		);
-		fetchAndInject(
-			(path = "components/candidate-inbox-shortcut.html"),
-			(target = targetContainer)
-		);
-		fetchAndInject(
-			(path = "components/workflows-shortcut.html"),
-			(target = targetContainer)
-		);
-	}
+    let targetContainer =
+      document.querySelector("header#app-header section.el-container") ||
+      document.querySelector("body.cem-site.desktop.has-toolbar.has-header");
+
+    targetContainer.insertAdjacentElement("afterbegin", shortcutContainer);
+
+    // Inject all components
+    for (const file of componentFiles) {
+      fetchAndInject(`./html/components/${file}`, targetContainer);
+    }
+  }
 }, 500);
